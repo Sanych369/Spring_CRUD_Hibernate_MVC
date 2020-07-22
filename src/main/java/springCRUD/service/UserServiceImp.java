32f71dc17ff4c9a3996b48f010de6949aa112186
Sanych369 <killa0007@mail.ru>
2020-07-23 02:34:14 +0600

    v1.0

---
+++ src/main/java/springCRUD/service/UserServiceImp.java
@@ -0,0 +1,46 @@
+package springCRUD.service;
+
+import springCRUD.DAO.UserDAO;
+import springCRUD.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class UserServiceImp implements UserService {
+
+    @Autowired
+    private UserDAO userDAO;
+
+    @Override
+    @Transactional
+    public void addUser(User user) {
+        userDAO.addUser(user);
+    }
+
+    @Override
+    @Transactional
+    public void deleteUser(Long id) {
+        userDAO.deleteUser(id);
+    }
+
+    @Override
+    @Transactional
+    public void updateUser(User user) {
+        userDAO.updateUser(user);
+    }
+
+    @Override
+    @Transactional
+    public List<User> getAllUsers() {
+        return userDAO.getAllUsers();
+    }
+
+    @Override
+    @Transactional
+    public User getUserById(Long id) {
+        return userDAO.getUserById(id);
+    }
+}
